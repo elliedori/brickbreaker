a35696619b8d153d21e7f1c4db0051c3249482b6
elliedori <elliedori@gmail.com>
2016-10-30 16:22:45 -0700

    Add code to change ball color on each collision

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
+var colors = ["#1b85b8", "  #5a5255", "#559e83", "#ae5a41", "#c3cb71"]
 
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var color = updateColor()
 
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
 
+function updateColor() {
+  color = colors[Math.floor(Math.random()*colors.length)]
+  return color;
+}
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
-  // reverse ball direction if hit it hits the top/bottom edges
+  // reverse ball direction if hit it hits the side edges
   if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
+    color = updateColor();
   }
-  // reverse ball direction if hit it hits the side edges
+  // reverse ball direction if hit it hits the top/bottom edges
   if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-  dy = -dy;
+    dy = -dy;
+    color = updateColor();
   }
 
   x += dx;
