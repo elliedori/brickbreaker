f78c851bea2aee99584cf2329561b24ce9ef4971
elliedori <elliedori@gmail.com>
2016-10-30 15:58:36 -0700

    Add notes file, update js file to include screen clearing before each ball re-draw

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
 
-ctx.beginPath();
-// first two coordinates specify top left corner of rectangle,
-// second two specify width & height
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
 
-ctx.beginPath();
-// coordinates of circle center, arc radius, start/end angle, 
-// optional param of direction to draw in
-ctx.arc(240, 160, 40, 0, Math.PI*2, false);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
---
+++ notes.js
@@ -0,0 +1,19 @@
+// draw a rectangle
+
+ctx.beginPath();
+// first two coordinates specify top left corner of rectangle,
+// second two specify width & height
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = '#FF0000';
+ctx.fill();
+ctx.closePath();
+
+// draw a circle
+
+ctx.beginPath();
+// coordinates of circle center, arc radius, start/end angle, 
+// optional param of direction to draw in
+ctx.arc(240, 160, 40, 0, Math.PI*2, false);
+ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+ctx.stroke();
+ctx.closePath();
