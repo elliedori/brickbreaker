10d3182490de9a4db6742266c232a50707a660a3
elliedori <elliedori@gmail.com>
2016-10-30 19:45:44 -0700

    Add listener to map paddle position to position of the mouse

--- app.js
+++ app.js
@@ -162,6 +162,15 @@ function keyUpHandler(e) {
   }
 }
 
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth/2;
+  }
+}
+
 setInterval(draw, 10);
 
 
